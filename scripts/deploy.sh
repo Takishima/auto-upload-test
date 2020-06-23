29a9d84ee09a71b1756ce6edb3530a5eb20fe5d5
Damien Nguyen <damien1@huawei.com>
2020-06-23 10:59:44 +0200

    Use script provider instead of Pypi

---
+++ scripts/deploy.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+section_start()
+{
+    export CURRENT_SECTION=$1
+    travis_fold start $CURRENT_SECTION
+    travis_time_start
+}
+
+section_finish()
+{
+    travis_time_finish
+    travis_fold stop $CURRENT_SECTION
+}
+
+# ------------------------------------------------------------------------------
+
+DIST_DIR=$1
+shift
+
+# ==============================================================================
+
+section_start "deploy.init"
+cat << EOF > ~/.pypirc
+[distutils]
+index-servers=
+    pypi
+
+[pypi]
+repository: $DEPLOY_URL
+username: $DEPLOY_USERNAME
+password: $DEPLOY_PASSWORD
+EOF
+section_finish
+
+section_start "deploy.build"
+python3 setup.py sdist
+python3 -m cibuildwheel --output-dir dist/
+setion_finish
+
+section_start "deploy.check"
+python3 -m twine check $DIST_DIR
+setion_finish
+
+section_start "deploy.upload"
+python3 -m twine upload "$@" -r pypi dist/*
+setion_finish
+
--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, Extension
 setup(
     name="auto-install-test",
     ext_modules=[Extension('example', sources=['example.cpp'])],
+    long_description='Testing CI upload to Pypi',
+    long_description_content_type='text/x-rst',
     version="0.1.0",
 )
