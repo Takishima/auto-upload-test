162570a1b622292510ac7a5d6342295bfde16170
Damien Nguyen <damien1@huawei.com>
2020-06-23 16:03:01 +0200

    New attempt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,28 +17,18 @@ username: $DEPLOY_USERNAME
 password: $DEPLOY_PASSWORD
 EOF
 
-PYTHON=python3
-if [[ ! -e $PYTHON ]]; then
-    PYTHON=python
-fi
-
-echo $($PYTHON --version)
-
-echo 'Installing dependencies'
-$PYTHON -m pip install -U wheel cibuildwheel twine
-
 echo 'Building source distribution'
-$PYTHON setup.py sdist
+python3 setup.py sdist
 
 echo 'Building binary distributions'
-$PYTHON -m cibuildwheel --output-dir dist/
+python3 -m cibuildwheel --output-dir dist/
 
 # Make sure that twine is installed (mostly for Mac OS)
-$PYTHON -m pip install twine
+python3 -m pip install twine
 
 echo 'Running twine check'
-$PYTHON -m twine check $DIST_DIR
+python3 -m twine check $DIST_DIR
 
 echo 'Running twine upload'
-$PYTHON -m twine upload "$@" -r pypi dist/*
+python3 -m twine upload "$@" -r pypi dist/*
 
