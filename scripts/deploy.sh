0b21d6ecffb894fce677bd8ee599c51027591c7a
Damien Nguyen <damien1@huawei.com>
2020-06-23 12:44:33 +0200

    Fix python on Windows

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,17 +18,17 @@ password: $DEPLOY_PASSWORD
 EOF
 
 echo 'Building source distribution'
-python3 setup.py sdist
+$PYTHON_EXEC setup.py sdist
 
 echo 'Building binary distributions'
-python3 -m cibuildwheel --output-dir dist/
+$PYTHON_EXEC -m cibuildwheel --output-dir dist/
 
 # Make sure that twine is installed (mostly for Mac OS)
-python3 -m pip install twine
+$PYTHON_EXEC -m pip install twine
 
 echo 'Running twine check'
-python3 -m twine check $DIST_DIR
+$PYTHON_EXEC -m twine check $DIST_DIR
 
 echo 'Running twine upload'
-python3 -m twine upload "$@" -r pypi dist/*
+$PYTHON_EXEC -m twine upload "$@" -r pypi dist/*
 
