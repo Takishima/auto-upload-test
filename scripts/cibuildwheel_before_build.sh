fec17a52f459ffc19352c009889c2118f9d1cdf1
Damien Nguyen <damien1@huawei.com>
2020-06-24 14:01:10 +0200

    Update build requirements

--- scripts/cibuildwheel_before_build.sh
+++ scripts/cibuildwheel_before_build.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
-yum install -y libpng-devel freetype-devel
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r build_requirements.txt
+
+# Required to build matplotlib with i686 images
+# yum install -y libpng-devel freetype-devel
+# python3 -m pip install -r requirements.txt
