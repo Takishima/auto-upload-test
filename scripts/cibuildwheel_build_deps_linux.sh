1970394dacb5c8b5440938c9509356330fbc4698
Damien Nguyen <damien1@huawei.com>
2020-06-25 12:22:30 +0200

    Update CIBuildWheel before install script for Pypy wheel

--- scripts/cibuildwheel_build_deps_linux.sh
+++ scripts/cibuildwheel_build_deps_linux.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
 
-echo $(python --version)
-python -m pip install -r build_requirements.txt
+if hash python3 2>/dev/null; then
+    PYTHON=python3
+else
+    PYTHON=python
+fi
+
+echo $($PYTHON --version)
+
+PYTHON_VERSION_MAJOR=$($PYTHON --version | cut -d ' ' -f2 | cut -d '.' -f1)
+if [ $PYTHON_VERSION_MAJOR -lt 3 ]; then
+    echo 'Python version major must be at least 3!'
+    exit 1
+fi
+
+$PYTHON -m pip install -r build_requirements.txt
 
 # Required to build matplotlib with i686 images
 # yum install -y libpng-devel freetype-devel
-# python3 -m pip install -r requirements.txt
+# $PYTHON -m pip install -r requirements.txt
